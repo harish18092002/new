3ba9c5ef41a13261284a66142cf27ad2aa3a7c39
Harish A <harish18092002@gmail.com>
2023-02-22 12:35:40 +0530

    new

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAoXlvBp3GmEpC2uIrATjYhOpCgTsIH7zE",
+        authDomain: "dbfire-e76c8.firebaseapp.com",
+        projectId: "dbfire-e76c8",
+        storageBucket: "dbfire-e76c8.appspot.com",
+        messagingSenderId: "988354295477",
+        appId: "1:988354295477:web:aff3109f91cfd6420db0fc"
+    }
+};
